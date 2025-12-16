00a54d5073a5145252e888d5fb9b82007df96e54
Emin Yardımcı <98025939+eminyardimci@users.noreply.github.com>
2025-12-16 22:31:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,13 +15,23 @@ const sections = {
         title: 'TÜBİTAK-2242',
         description: 'TÜBİTAK 2242 Yarışması\'nda İmalat ve Otomotiv kategorisinde takım olarak beşincilik elde ettik. Projede Bluetooth ile Arduino tabanlı kontrol sistemi geliştirdim, mekanizma tasarımı ve sensör entegrasyonu gerçekleştirdim. Parçaların 3D baskısını yaparak tavana monte edilen raylı sistem üzerinde hareket edebilen, mutfaklarda kullanılmak üzere yardımcı robot kol prototipi geliştirdik.'
     },
-    robotaksi: {
-        year: '2023-2025',
-        description: '2023\'te ekibimizle 3. olarak En İyi Takım Ruhu ödülünü kazandık. Elektronik kart tasarımı, elektrik sistemi ve gömülü sistem geliştirmeden sorumluydum. 2024\'te takım kaptanı olarak önceki aracı baştan tasarladık ve ikinci bir araç ürettik; sponsorluklar sağladım. 2025\'te yeni bir araç tasarladık, Cybertack\'tan esinlenerek gömülü sistem yazılımı ve elektronik kart tasarımı yürüttüm.',
-        title: 'Robotaksi Yarışması'
+    robotaksi2023: {
+        year: '2023',
+        title: 'Robotaksi Yarışması',
+        description: '2023 Robotaksi Binek Otonom Araç Yarışması\'nda ekibimizle 3. olarak En İyi Takım Ruhu ödülünü kazandık. Bu projede ben aracın elektronik kart tasarımı, elektrik sistemi ve gömülü sistem geliştirme lerinden sorumluydum; sensör ve aktüatörlerin entegrasyonu ile aracın güvenli ve verimli çalışmasını sağladım.'
+    },
+    robotaksi2024: {
+        year: '2024',
+        title: 'Robotaksi Yarışması',
+        description: '2024 yılında Robotaksi projesinde takım kaptanı olarak önceki aracımızı hem elektriksel hem mekanik olarak baştan tasarlayıp geliştirilmesinde bulundum, üzerine ikinci bir araç daha ürettik. Projede gömülü sistem yazılımı, elektronik kart tasarımı ve takım organizasyonu görevlerini üstlendim ve mekanik çalışmalara da katkı sağladım. Ayrıca Baykar, T.C. Gençlik ve Spor Bakanlığı, Uzay Spor, Ensar Kalıp, Mesan Elektrik ve Düzce Belediyesi ile görüşmeler yaparak sponsorluk anlaşmalarını sağladım.'
+    },
+    robotaksi2025: {
+        year: '2025',
+        title: 'Robotaksi Yarışması',
+        description: '2025 yılında Robotaksi projesinde hem mekanik çalışmalara destek sağladım hem de elektrik ekibinde görev aldım; bu süreçte yeni bir araç tasarlayıp geliştirdik. Araç tasarımında Cybertack\'tan esinlenerek yola çıktık ve projede gömülü sistem yazılımı, elektronik kart tasarımı görevlerini yürüttüm.'
     },
     robotkol: {
-        year: '2023-24',
+        year: '2023-2024',
         title: 'Robot Kol',
         description: 'Sonel Yazılım firmasında bir yıl boyunca, KOSGEB destekli robot kol projesi kapsamında, robot kol prototipi üretimi ve gömülü sistem kodlarının geliştirilmesi alanlarında çalıştım.'
     },
@@ -31,7 +41,7 @@ const sections = {
         description: 'Düzce Üniversitesi Mekatronik Mühendisliği bölümüne kazandırılan PLC Atölyesi için PLC deney setleri, FESTO deney standları ve sıvı seviye kontrol mekanizması üzerinde hem PLC programlama hem de mekanik tasarım ve montaj çalışmalarında aktif olarak görev aldım.'
     },
     egitmenlik: {
-        year: '2023-25',
+        year: '2023-2025',
         title: 'Eğitmenlik',
         description: 'T3 Vakfı Deneyap Atölyelerinde ileri Robotik, Nesnelerin İnterneti ve Elektronik Programlama, Nano Teknoloji, Tasarım ve Üretim, Uçan ve Otonom Araçlar alanlarında eğitmenlik yaptım.'
     },
