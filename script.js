5669cc5e879debca6bcb12fe5430f20aacc042ba
Emin Yardımcı <98025939+eminyardimci@users.noreply.github.com>
2025-12-16 19:35:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function openProjects() {
+  alert("Projeler ekranı bir sonraki adımda açılacak 👍");
+}
+
+/*function openProjects() {
   document.getElementById("projects-overlay").style.display = "flex";
 }
 
 function closeProjects() {
   document.getElementById("projects-overlay").style.display = "none";
 }
+*/
