39426e55e68998aba0d4c644c82b73ba30faded7
Emin Yardımcı <98025939+eminyardimci@users.noreply.github.com>
2025-12-16 19:03:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,28 +5,3 @@ function openProjects() {
 function closeProjects() {
   document.getElementById("projects-overlay").style.display = "none";
 }
-
-const data = {
-  robotaksi: {
-    title: "Robotaksi",
-    img: "assets/projects/robotaksi.jpg",
-    desc: "Otonom araç yarışmaları için geliştirilen elektronik ve gömülü sistemler."
-  },
-  tubitak: {
-    title: "TÜBİTAK",
-    img: "assets/projects/tubitak.jpg",
-    desc: "Bluetooth kontrollü yardımcı robot kol projesi."
-  }
-  // diğerlerini aynı şekilde eklersin
-};
-
-function openModal(key) {
-  document.getElementById("modal").style.display = "flex";
-  document.getElementById("modal-title").innerText = data[key].title;
-  document.getElementById("modal-img").src = data[key].img;
-  document.getElementById("modal-desc").innerText = data[key].desc;
-}
-
-function closeModal() {
-  document.getElementById("modal").style.display = "none";
-}
